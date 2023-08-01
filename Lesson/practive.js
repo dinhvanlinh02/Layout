c7042ae0069592677e1eafb94683bdf2682d4bfb
dinhvanlinh <dinhvanlinh06052002@gmail.com>
2023-08-01 11:04:30 +0700

    update

---
+++ Lesson/practive.js
@@ -0,0 +1,49 @@
+//bai1
+/*var arr = [1,5,7,8,9,15,8]
+
+for(var i = 0;i<arr.length;i++){
+    var item = arr[i];
+    if(item%2==0){
+        console.log(item)
+    }
+}
+
+var arr2 = [1,6,8,7,6,9]
+console.log(arr2.length);
+console.log(arr2[0]);
+console.log(arr2[arr2.length-1]);
+arr2.push(5);
+ console.log(arr2);
+ var ten = 10;
+ arr2[0] = 10;
+ arr2[arr2.length-1] = ten;
+ console.log(arr2);
+ 
+var nhietdo = 150;
+if(20<nhietdo&&nhietdo<100){
+    console.log('nhiet do du dung');
+}
+if(20>nhietdo){
+    console.log('tang nhiet do ');
+}
+if(nhietdo>100){
+    console.log('ha nhiet do');
+}
+
+var obj = {
+    firstname : 'dinh',
+    lastname : 'linh'
+
+}
+console.log('Ho va ten hoc vien la :'+obj.firstname +' '+obj.lastname);
+*/
+
+var car = {
+    hangxe : 'BMW',
+    gia:30000,
+    xuatxu:'duc'
+}
+car.mausac = 'den'
+car.gia=250000
+console.log(car);
+
